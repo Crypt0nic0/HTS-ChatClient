19251615ec606764def877bbac022aea25514bc9
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-13 16:16:28 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, 2345);
+
+try
+{
+    socket.Connect(endpoint);
+
+    Thread t = new Thread(LireMessages);
+    t.IsBackground = true;
+    t.Start(socket);
+
+    while (true)
+    {
+        string? message = Console.ReadLine();
+        if (message == "q") { break; }
+        if (!string.IsNullOrEmpty(message))
+        {
+            var buffer = Encoding.UTF8.GetBytes(message);
+            socket.Send(buffer);
+        }
+    }
+}
+catch
+{
+    Console.WriteLine("Le serveur est injoignable");
+}
+finally
+{
+    if (socket.Connected)
+    {
+        socket.Shutdown(SocketShutdown.Both);
+    }
+
+    socket.Close();
+}
+
+
+void LireMessages(object? obj)
+{
+    if (obj is Socket s)
+    {
+        while (true)
+        {
+            byte[] buffer = new byte[4096];
+            int read = socket.Receive(buffer);
+            if (read > 0)
+            {
+                var message = Encoding.UTF8.GetString(buffer, 0, read);
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
